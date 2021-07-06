225032194685a63d5be5756d36854460cfb7034e
Yuval Aviguy <yuvala@Yuvals-MacBook-Pro.local>
2021-07-06 16:35:07 +0300

    export num utils to file

---
+++ numUtils.js
@@ -0,0 +1,32 @@
+const asc = arr => arr.sort((a, b) => a - b);
+const sum = arr => arr.reduce((a, b) => a + b, 0);
+/////////////////////////////////////
+module.exports = {
+  nrmlFloat: function(f){    
+    if(isNaN(f)){
+      return -1;
+    }
+    while( f.toString().indexOf('+') > -1 ){
+      f /= 10;
+    }
+
+    return parseFloat(f.toFixed(8));
+  },
+  asc: asc,
+  quantile: (arr, q) => {
+    const sorted = asc(arr);
+    const pos = (sorted.length - 1) * q;
+    const base = Math.floor(pos);
+    const rest = pos - base;
+    if (sorted[base + 1] !== undefined) {
+        return sorted[base] + rest * (sorted[base + 1] - sorted[base]);
+    } else {
+        return sorted[base];
+    }
+  },
+  sum: sum,
+  std: (arr, mu) => {  
+    const diffArr = arr.map(a => (a - mu) ** 2);
+    return Math.sqrt(sum(diffArr) / (arr.length - 1));
+  }
+};
