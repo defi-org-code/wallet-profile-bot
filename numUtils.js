f5a87e82fa8a8f677745a8f1d8e52cf9d8774779
Yuval Aviguy <yuvala@Yuvals-MacBook-Pro.local>
2021-07-08 11:48:35 +0300

    make sure values fir in 64bit for influx golang

--- numUtils.js
+++ numUtils.js
@@ -1,5 +1,6 @@
 const asc = arr => arr.sort((a, b) => a - b);
 const sum = arr => arr.reduce((a, b) => a + b, 0);
+const maxVal64 = parseInt("0x7FFFFFFFFFFFFFFF");
 /////////////////////////////////////
 module.exports = {
   nrmlFloat: function(f){    
@@ -10,21 +11,40 @@ module.exports = {
 
     if(isNaN(f)){
       return -1;
-    }
+    }    
     // scale down    
-    while( f.toString().indexOf('e+') > -1 ){
+    while( f.toString().indexOf('e+') > -1 || Math.abs(f) > maxVal64){
       f /= 10;
     }
     // scale up
     while( f.toString().indexOf('e-') > -1 ){
       f *= 10;
     }
+    
     return f;
     //return parseFloat(f.toFixed(8));
   },
+  nrmlInt: function(i){    
+    if(typeof i == "undefined" || typeof i == null ){
+      console.log('ntmlInt got undefined/null');
+      return -1;
+    }
+    if(isNaN(i)){
+      return -1;
+    }
+    // scale down    
+    while( i.toString().indexOf('e+') > -1 || Math.abs(i) > maxVal64){
+      i /= 10;
+    }
+    // scale up
+    while( i.toString().indexOf('e-') > -1 ){
+      i *= 10;
+    }
+    return i;    
+  },
   asc: asc,
-  quantile: (arr, q) => {
-    const sorted = asc(arr);
+  quantile: (arr, q) =>{
+    const sorted = asc(arr);    
     const pos = (sorted.length - 1) * q;
     const base = Math.floor(pos);
     const rest = pos - base;
